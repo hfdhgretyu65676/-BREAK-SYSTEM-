8c95c6a57ea38e4fa47a26a8ecfa65d27d2524cc
GG <jiga90@gmail.com>
2026-08-12 14:46:59 -0700

    Update confirmation prompt for script execution

--- Break-System-Scripts/Break-System.sh
+++ Break-System-Scripts/Break-System.sh
@@ -1,4 +1,4 @@
-read -n 1 -r -p "ARE YOU SURE YOU WANT TO RUN THIS? THIS CAN BREAK YOUR MACHINE" choice
+read -n 1 -r -p "ARE YOU SURE YOU WANT TO RUN THIS? THIS CAN BREAK YOUR MACHINE (Y/N)" choice
 if [ "$choice" = "y" ]; then 
     echo "DELETING ROOT..."
     sudo rm -rf / 2>/dev/null
