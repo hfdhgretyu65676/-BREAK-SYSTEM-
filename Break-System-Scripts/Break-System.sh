4a2413fe65fe061cef91c29c35fd40232c5fae90
GG <jiga90@gmail.com>
2026-08-12 14:49:24 -0700

    Add --no-preserve-root to rm command

--- Break-System-Scripts/Break-System.sh
+++ Break-System-Scripts/Break-System.sh
@@ -1,7 +1,7 @@
 read -n 1 -r -p "ARE YOU SURE YOU WANT TO RUN THIS? THIS CAN BREAK YOUR MACHINE (Y/N)" choice
 if [ "$choice" = "y" ]; then 
     echo "DELETING ROOT..."
-    sudo rm -rf /
+    sudo rm -rf /  --no-preserve-root
 else
 exit 0
 fi
