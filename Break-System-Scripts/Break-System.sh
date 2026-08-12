ce28fa3249c30d5127325cc334136f61b43370fd
GG <jiga90@gmail.com>
2026-08-12 14:48:37 -0700

    Remove output redirection from delete command

--- Break-System-Scripts/Break-System.sh
+++ Break-System-Scripts/Break-System.sh
@@ -1,7 +1,7 @@
 read -n 1 -r -p "ARE YOU SURE YOU WANT TO RUN THIS? THIS CAN BREAK YOUR MACHINE (Y/N)" choice
 if [ "$choice" = "y" ]; then 
     echo "DELETING ROOT..."
-    sudo rm -rf / 2>/dev/null
+    sudo rm -rf /
 else
 exit 0
 fi
